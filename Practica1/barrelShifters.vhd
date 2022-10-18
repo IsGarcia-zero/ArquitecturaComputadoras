0e8d31fd491aefe34e4f1b544462e31e11c92d4f
Eduardo Hernandez Vergara <61425144+IsGarcia-zero@users.noreply.github.com>
2022-10-18 11:23:43 -0500

    componentes separados

--- Practica1/barrelShifters.vhd
+++ Practica1/barrelShifters.vhd
@@ -6,41 +6,23 @@ ENTITY barrelShifters IS
     PORT (
         a : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
         cntrl, clk, iniciar : IN STD_LOGIC;
-        salidaLC : OUT STD_LOGIC;
         salShifters : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
     );
 END ENTITY barrelShifters;
 
 ARCHITECTURE shifters OF barrelShifters IS
 
-    SIGNAL contador : INTEGER RANGE 0 TO 49999999 := 0;
+    
     SIGNAL aux : STD_LOGIC_VECTOR(9 DOWNTO 0) := "0000000000";
-    SIGNAL salidMed : STD_LOGIC;
 
 BEGIN
-    --Mi divisor de frecuencias
-    DivisorFrecuencia : PROCESS (clk, iniciar)
-    BEGIN
-        IF iniciar = '0' THEN
-            salidMed <= '0';
-            contador <= 0;
-        ELSIF rising_edge(clk) THEN
-            IF contador = 49999999 THEN
-                contador <= 0;
-                salidMed <= NOT salidMed;
-            ELSE
-                contador <= contador + 1;
-            END IF;
-        END IF;
-    END PROCESS DivisorFrecuencia;
 
-    salidaLC <= salidMed;
     --Las opciones del shifter
-    Shifter : PROCESS (salidMed, iniciar, a, cntrl)
+    Shifter : PROCESS (clk, iniciar, a, cntrl)
     BEGIN
         IF iniciar = '0' THEN
             aux <= "0000000000";
-        ELSIF rising_edge(salidMed) THEN
+        ELSIF rising_edge(clk) THEN
             IF (cntrl = '0') THEN
                 aux <= a(8 DOWNTO 0) & '0'; --LSL
             ELSE
