b97afbdbf2c3726ab42d3c7838b3c6531aada2bd
Eduardo Hernandez Vergara <61425144+IsGarcia-zero@users.noreply.github.com>
2022-10-19 13:19:55 -0500

    Mux

--- Practica1/mux.vhd
+++ Practica1/mux.vhd
@@ -3,9 +3,8 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 entity mux is
     port (
-        a, b : in std_logic_vector(9 downto 0);
-        sM, sA, sL: in std_logic_vector(1 downto 0);
-        sS: in std_logic;
+        a, s, l: in std_logic_vector(9 downto 0);
+        sM: in std_logic_vector(1 downto 0);
         salida: out std_logic_vector(9 downto 0)
     );
 end entity mux;
@@ -15,11 +14,12 @@ begin
     Multiplexacion: process(a, b, sM, salida)
     begin
     if sM = "00" then --Artimethic
-        aux <= salida;
+        aux <= a;
     elsif sM = "01" then --Shifter
-        
+        aux <= s;
     elsif sM = "10" then --Logic
-        
+        aux <= l;
     end if;
     end process Multiplexacion;
+    salida <= aux;
 end architecture muxArch;
