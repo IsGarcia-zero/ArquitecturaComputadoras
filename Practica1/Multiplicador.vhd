f69f3e5481592c6fe4d24304d2a8bdb0763b6e5c
Eduardo Hernandez Vergara <61425144+IsGarcia-zero@users.noreply.github.com>
2022-11-02 11:22:13 -0600

    se añadio el archivo

--- Practica1/Multiplicador.vhd
+++ Practica1/Multiplicador.vhd
@@ -4,10 +4,10 @@ USE IEEE.numeric_std.ALL;
 --Checa bien las entradas y las salidas de cada uno de los 
 --Archivos
 ENTITY Multiplicador IS PORT (
-		ope : IN STD_LOGIC;
-		VarA, VarB : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
-		Salida : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
-		carryF, overflowF, zeroF, sumF : OUT STD_LOGIC
+    ope : IN STD_LOGIC;
+    VarA, VarB : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+    Salida : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
+    carryF, overflowF, zeroF, sumF : OUT STD_LOGIC
 );
 END Multiplicador;
 
@@ -16,7 +16,7 @@ ARCHITECTURE Mult OF Multiplicador IS
     SIGNAL C : STD_LOGIC_VECTOR (24 DOWNTO 0);
     SIGNAL S_prime : STD_LOGIC_VECTOR (19 DOWNTO 0);
     SIGNAL CF, OvF, ZF, SF : STD_LOGIC_VECTOR (4 DOWNTO 0);
-	 SIGNAL Aux1, Aux2, Aux3, Aux4 : std_logic_vector (4 downto 0);
+    SIGNAL Aux1, Aux2, Aux3, Aux4 : STD_LOGIC_VECTOR (4 DOWNTO 0);
 
     COMPONENT Full_Adder_Five IS PORT (
         selector : IN STD_LOGIC;
@@ -61,19 +61,19 @@ BEGIN
     C(22) <= VarB(2) AND VarA(4);
     C(23) <= VarB(3) AND VarA(4);
     C(24) <= VarB(4) AND VarA(4);
-    
-	 Aux1 <= '0' & C(4 downto 1);							        
-    Sum1: Full_Adder_Five Port Map(ope ,Aux1 , C(9 downto   5), S_Prime(4  downto  0), CF(0), OvF(0), ZF(0), SF(0));
-		  
-	 Aux2 <= CF(0) & S_Prime(4 downto  1);
-	 Sum2: Full_Adder_Five Port Map(ope ,Aux2 , C(14 downto 10), S_Prime(9  downto  5), CF(1), OvF(1), ZF(1), SF(1));
-		  
-	 Aux3 <= CF(1) & S_Prime(9 downto  6);
-	 Sum3: Full_Adder_Five Port Map(ope ,Aux3 , C(19 downto 15), S_Prime(14 downto 10), CF(2), OvF(2), ZF(2), SF(2));
-		  
-	 Aux4 <= CF(2) & S_Prime(14 downto 11);
-	 Sum4: Full_Adder_Five Port Map(ope ,Aux4 , C(24 downto 20), S_Prime(19 downto 15), CF(3), OvF(3), ZF(3), SF(3));  
-		 
+
+    Aux1 <= '0' & C(4 DOWNTO 1);
+    Sum1 : Full_Adder_Five PORT MAP(ope, Aux1, C(9 DOWNTO 5), S_Prime(4 DOWNTO 0), CF(0), OvF(0), ZF(0), SF(0));
+
+    Aux2 <= CF(0) & S_Prime(4 DOWNTO 1);
+    Sum2 : Full_Adder_Five PORT MAP(ope, Aux2, C(14 DOWNTO 10), S_Prime(9 DOWNTO 5), CF(1), OvF(1), ZF(1), SF(1));
+
+    Aux3 <= CF(1) & S_Prime(9 DOWNTO 6);
+    Sum3 : Full_Adder_Five PORT MAP(ope, Aux3, C(19 DOWNTO 15), S_Prime(14 DOWNTO 10), CF(2), OvF(2), ZF(2), SF(2));
+
+    Aux4 <= CF(2) & S_Prime(14 DOWNTO 11);
+    Sum4 : Full_Adder_Five PORT MAP(ope, Aux4, C(24 DOWNTO 20), S_Prime(19 DOWNTO 15), CF(3), OvF(3), ZF(3), SF(3));
+
     Salida(0) <= C(4);
     Salida(1) <= S_Prime(4);
     Salida(2) <= S_Prime(9);
