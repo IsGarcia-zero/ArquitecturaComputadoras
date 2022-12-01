7e959741f2dec0ea0c54019593173488c88b341e
Erik Alcantara <ealcantarac1700@alumno.ipn.mx>
2022-11-30 22:16:37 -0600

    Correciones de pastrana
    Si incluyen saltos considerando flags y cmp

--- p3/ALU.vhd
+++ p3/ALU.vhd
@@ -20,7 +20,9 @@ ARCHITECTURE bhr OF ALU IS
 	SIGNAL rlj_aux : STD_LOGIC;
 	SIGNAL b_aux, c_aux, d_aux : STD_LOGIC;
 	SIGNAL a_aux : STD_LOGIC_VECTOR(1 DOWNTO 0);
+	SIGNAL z_aux, s_aux, ov_aux, cf_aux : STD_LOGIC;
 	SIGNAL gtt_aux,eqq_aux,ltt_aux : STD_LOGIC;
+	
 -- Llamamos a todos los componentes
 COMPONENT uapro IS
     PORT (
@@ -53,13 +55,13 @@ COMPONENT barrelShifters IS
     );
 END COMPONENT barrelShifters;
 
-COMPONENT comparador8 IS
+COMPONENT comparador16 IS
     PORT (
-        x, y: in std_logic_vector(7 downto 0);
+        x, y: in std_logic_vector(15 downto 0);
         g0, l0: in std_logic;
         gtt, eqq, ltt: out std_logic
     );
-END COMPONENT comparador8;
+END COMPONENT comparador16;
 
 BEGIN
 	
@@ -101,18 +103,22 @@ BEGIN
 				R <= ua_sal;
 			WHEN "1011" => --Comparacion
 				sel_aux <= "00";
-				R <= "0000000000000" & gtt_aux & eqq_aux & ltt_aux;
+				R <= "000000000" & cf_aux & z_aux & ov_aux & s_aux & gtt_aux & eqq_aux & ltt_aux;
 			WHEN OTHERS => 
 				R <= "1111111111111111";
 				sel_aux <= "00";
 		END CASE;
+		c_flag <= cf_aux;
+		z_flag <=  z_aux;
+		ov_flag <= ov_aux;
+		s_flag <= s_aux;
 	END PROCESS;
 	
 
-unidad_aritmetica: uapro PORT MAP(A(14 DOWNTO 0),B(14 DOWNTO 0),sel_aux, clk,rst,A(15),B(15),ua_sal,c_flag,z_flag,ov_flag,s_flag);
+unidad_aritmetica: uapro PORT MAP(A(14 DOWNTO 0),B(14 DOWNTO 0),sel_aux, clk,rst,A(15),B(15),ua_sal,cf_aux,z_aux,ov_aux,s_aux);
 unidad_logica: Logicas PORT MAP(A,B, sel_aux, clk, log_sal,a_aux,b_aux);
 barrel_shifters: barrelShifters PORT MAP(A,sel_aux_2,clk, '1', bar_sal, d_aux);
-comparador: comparador8 PORT MAP(A(7 DOWNTO 0),B(7 DOWNTO 0),'0','0',gtt_aux,eqq_aux,ltt_aux);
+comparador: comparador16 PORT MAP(A,B,'0','0',gtt_aux,eqq_aux,ltt_aux);
 
 
 END ARCHITECTURE;
--- p3/datapath.vhd
+++ p3/datapath.vhd
@@ -9,7 +9,7 @@ ENTITY datapath IS
 		ecuacion : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
 		clk, rst : IN STD_LOGIC;
 		salida, s_p1, s_p2 : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-		ins : OUT STD_LOGIC_VECTOR(11 DOWNTO 0);
+		ins : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
 		z_flag, s_flag, ov_flag, c_flag : OUT STD_LOGIC
 	);
 END ENTITY datapath;
@@ -22,7 +22,7 @@ ARCHITECTURE bhr OF datapath IS
 	SIGNAL REG_A,REG_B,REG_C,REG_D : STD_LOGIC_VECTOR(15 DOWNTO 0);
 	SIGNAL PC, PC_AUX : INTEGER RANGE 0 TO 9999 := 0;
 	
-	SIGNAL MAR, IR : STD_LOGIC_VECTOR(11 DOWNTO 0);
+	SIGNAL MAR, IR : STD_LOGIC_VECTOR(15 DOWNTO 0);
 	SIGNAL ACC: STD_LOGIC_VECTOR(9 DOWNTO 0);
 	SIGNAL MBR : STD_LOGIC_VECTOR(15 DOWNTO 0);
 
@@ -30,7 +30,8 @@ ARCHITECTURE bhr OF datapath IS
 	SIGNAL buff2: STD_LOGIC_VECTOR(7 DOWNTO 0);
 
 	TYPE data IS ARRAY (11 DOWNTO 0) OF STD_LOGIC_VECTOR(9 DOWNTO 0);
-	TYPE list IS ARRAY (39 DOWNTO 0) OF STD_LOGIC_VECTOR(11 DOWNTO 0);
+	TYPE cmp IS ARRAY (10 DOWNTO 0) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
+	TYPE list IS ARRAY (39 DOWNTO 0) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
 	TYPE reg IS ARRAY(0 TO 7) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
 	SIGNAL reggy, reggu : reg;
 	SIGNAL zflag, sflag, ovflag, cflag : STD_LOGIC;
@@ -41,10 +42,11 @@ ARCHITECTURE bhr OF datapath IS
 	SIGNAL signo1, signo2 : STD_LOGIC :='0';
 	
 	CONSTANT values : data := (
-		0 => "0000000110", -- X
-		1 => "0000001001", -- Y
-		2 => "0000000111", -- Z
-		3 => "0000001010", -- W
+	-- Datos precargados en la "ROM" para usar despues en ecuacines
+		0 => "0000000110", -- X (6)
+		1 => "0000001001", -- Y (9)
+		2 => "0000000111", -- Z (7)
+		3 => "0000001010", -- W (10)
 		4 => "0000010001", -- 17
 		5 => "0000011001", -- 25
 		6 => "0000000100", -- -4
@@ -55,69 +57,70 @@ ARCHITECTURE bhr OF datapath IS
 		11 => "0000000000"
 	);
 	
-	CONSTANT jmps : data := (
-		0 => "0100011010", -- X
-		1 => "0001101011", -- Y
-		2 => "0100011010", -- Z
-		3 => "0010011101", -- W
-		4 => "0000010001", -- 17
-		5 => "0000011001", -- 25
-		6 => "0000000100", -- -4
-		7 => "0000001010", -- 10
-		8 => "0000011110", -- 30
-		9 => "0000000010", -- -2
-		10 => "1000000111", -- -7
-		11 => "0000000000"
+	CONSTANT cmprs : cmp := (
+	--Aqui van almacenadas las "configuraciones" de el comparador y de las flags
+	--Carry, Zero, Overflow, Sign, Mayor, Igual, Menor
+		0  => "0000000000000001", -- El numero es menor
+		1  => "0000000000000010", -- El numero es igual
+		2  => "0000000000000011", -- El numero es menor o igual
+		3  => "0000000000000100", -- El numero es mayor
+		4  => "0000000000000101", -- El numero es diferente
+		5  => "0000000000000110", -- El numero es mayor o igual
+		6  => "0000000000001000", -- El numero es negativo
+		7  => "0000000000010000", -- El numero es muy grande
+		8  => "0000000000100000", -- El numero es cero
+		9  => "0000000001000000", -- El numero tiene un bit extra
+		10 => "0000000000000000"  -- Salto directo sin comprobacion
 	);
 	
 	
 	CONSTANT INSTRUCTIONS : list := (
 	
-		-- ECUACION 1 (17x+25y-w/4)
-		0 => ("1101"&"0000"&"0000"), -- LOAD R1 <- X
-		1 => ("1101"&"0001"&"0100"), -- LOAD R2 <- 17 
-		2 => ("1001"&"0000"&"0001"), -- MULT R1  R2
-		3 => ("1101"&"0010"&"0001"), -- LOAD R3 <- Y
-		4 => ("1101"&"0001"&"0101"), -- LOAD R2 <- 25 
-		5 => ("1001"&"0010"&"0001"), -- MULT R3  R2
-		6 => ("1101"&"0011"&"0011"), -- LOAD R4 <- W
-		7 => ("1101"&"0001"&"0110"), -- LOAD R2 <- -4 
-		8 => ("1010"&"0011"&"0001"), -- DIV R4  R2
-		9 => ("0111"&"0000"&"0010"), -- SUM R1  R3
-		10 => ("1000"&"0000"&"0011"), -- RES R1  R4
-		11 => ("1111"&"0000"&"0000"), -- NOT OP
+		-- ECUACION 1: (17x+25y-w/4)
+		0 => ("0000"&"1101"&"0000"&"0000"), -- LOAD R1 <- X
+		1 => ("0000"&"1101"&"0001"&"0100"), -- LOAD R2 <- 17 
+		2 => ("0000"&"1001"&"0000"&"0001"), -- MULT R1  R2
+		3 => ("0000"&"1101"&"0010"&"0001"), -- LOAD R3 <- Y
+		4 => ("0000"&"1101"&"0001"&"0101"), -- LOAD R2 <- 25 
+		5 => ("0000"&"1001"&"0010"&"0001"), -- MULT R3  R2
+		6 => ("0000"&"1101"&"0011"&"0011"), -- LOAD R4 <- W
+		7 => ("0000"&"1101"&"0001"&"0110"), -- LOAD R2 <- -4 
+		8 => ("0000"&"1010"&"0011"&"0001"), -- DIV R4  R2
+		9 => ("0000"&"0111"&"0000"&"0010"), -- SUM R1  R3
+		10 => ("0000"&"1000"&"0000"&"0011"), -- RES R1  R4
+		11 => ("0000"&"1111"&"0000"&"0000"), -- NOT OP
 		
-		-- ECUACION 2 (10x**2+30x-z/2)0100011010
-		12 => ("1101"&"0000"&"0000"), -- LOAD R1 <- X
-		13 => ("1101"&"0001"&"0000"), -- LOAD R2 <- X
-		14 => ("1001"&"0000"&"0001"), -- MULT R1  R2
-		15 => ("1101"&"0100"&"0111"), -- LOAD R5  10
-		16 => ("1001"&"0000"&"0100"), -- MUL R1  R5
-		17 => ("1101"&"0010"&"0000"), -- LOAD R3 <- X
-		18 => ("1101"&"0001"&"1000"), -- LOAD R2 <- 30
-		19 => ("1001"&"0010"&"0001"), -- MULT R3  R2
-		20 => ("1101"&"0011"&"0010"), -- LOAD R4 <- Z
-		21 => ("1101"&"0001"&"1001"), -- LOAD R2 <- -2
-		22 => ("1010"&"0011"&"0001"), -- DIV R4  R2
-		23 => ("0111"&"0000"&"0010"), -- SUM R1  R3
-		24 => ("1000"&"0000"&"0011"), -- RES R1  R4
-		25 => ("1111"&"0000"&"0000"), -- NOT OP
+		-- ECUACION 2: (10x**2+30x-z/2)
+		12 => ("0000"&"1101"&"0000"&"0000"), -- LOAD R1 <- X
+		13 => ("0000"&"1101"&"0001"&"0000"), -- LOAD R2 <- X
+		14 => ("0000"&"1001"&"0000"&"0001"), -- MULT R1  R2
+		15 => ("0000"&"1101"&"0100"&"0111"), -- LOAD R5  10
+		16 => ("0000"&"1001"&"0000"&"0100"), -- MUL R1  R5
+		17 => ("0000"&"1101"&"0010"&"0000"), -- LOAD R3 <- X
+		18 => ("0000"&"1101"&"0001"&"1000"), -- LOAD R2 <- 30
+		19 => ("0000"&"1001"&"0010"&"0001"), -- MULT R3  R2
+		20 => ("0000"&"1101"&"0011"&"0010"), -- LOAD R4 <- Z
+		21 => ("0000"&"1101"&"0001"&"1001"), -- LOAD R2 <- -2
+		22 => ("0000"&"1010"&"0011"&"0001"), -- DIV R4  R2
+		23 => ("0000"&"0111"&"0000"&"0010"), -- SUM R1  R3
+		24 => ("0000"&"1000"&"0000"&"0011"), -- RES R1  R4
+		25 => ("0000"&"1111"&"0000"&"0000"), -- NOT OP
 		
-		-- ECUACION (-x**3-7z+w/10)
-		26 => ("1101"&"0000"&"0000"), -- LOAD R1 <- X
-		27 => ("1101"&"0001"&"0000"), -- LOAD R2 <- X
-		28 => ("1001"&"0000"&"0001"), -- MULT R1  R2
-		29 => ("1101"&"0001"&"0000"), -- LOAD R2 <- X
-		30 => ("1001"&"0000"&"0001"), -- MULT R1  R2
-		31 => ("1101"&"0010"&"0010"), -- LOAD R3 <- z
-		32 => ("1101"&"0001"&"1010"), -- LOAD R2 <- -7
-		33 => ("1001"&"0010"&"0001"), -- MULT R3  R2
-		34 => ("1101"&"0011"&"0011"), -- LOAD R4 <- w
-		35 => ("1101"&"0001"&"0111"), -- LOAD R2 <- 10
-		36 => ("1010"&"0011"&"0001"), -- DIV R4  R2
-		37 => ("1000"&"0000"&"0010"), -- SUM R1  R3
-		38 => ("0111"&"0000"&"0011"), -- RES R1  R4
-		39 => ("1111"&"0000"&"0000") -- NOT OP
+		-- ECUACION 3: (-x**3-7z+w/10)
+		26 => ("0000"&"1101"&"0000"&"0000"), -- LOAD R1 <- X
+		27 => ("0000"&"1101"&"0001"&"0000"), -- LOAD R2 <- X
+		28 => ("0000"&"1001"&"0000"&"0001"), -- MULT R1  R2
+		29 => ("0000"&"1101"&"0001"&"0000"), -- LOAD R2 <- X
+		30 => ("0000"&"1001"&"0000"&"0001"), -- MULT R1  R2
+		31 => ("0000"&"1101"&"0010"&"0010"), -- LOAD R3 <- z
+		32 => ("0000"&"1101"&"0001"&"1010"), -- LOAD R2 <- -7
+		33 => ("0000"&"1001"&"0010"&"0001"), -- MULT R3  R2
+		34 => ("0000"&"1101"&"0011"&"0011"), -- LOAD R4 <- w
+		35 => ("0000"&"1101"&"0001"&"0111"), -- LOAD R2 <- 10
+		36 => ("0000"&"1010"&"0011"&"0001"), -- DIV R4  R2
+		37 => ("0000"&"1000"&"0000"&"0010"), -- SUM R1  R3
+		38 => ("0000"&"0111"&"0000"&"0011"), -- RES R1  R4
+		39 => ("0000"&"1111"&"0000"&"0000") -- NOT OP
 	);
 	
 	COMPONENT ALU IS 
@@ -142,7 +145,7 @@ BEGIN
 			CASE pr_state IS
 				WHEN state0 => -- FETCH
 					CASE ecuacion IS
-						WHEN "00" => 
+						WHEN "00" =>  -- Comienza la ecuacion 1
 							IF (PC < 11) THEN
 								PC <= PC + 1;
 								pr_state <= state1;
@@ -164,7 +167,7 @@ BEGIN
 							END IF;
 							MAR <= INSTRUCTIONS(PC);	
 							
-						WHEN "01" => 
+						WHEN "01" => --Comienza la ecuacion 2
 							IF (PC < 11) THEN
 								PC <= 12;
 							ELSE
@@ -189,7 +192,7 @@ BEGIN
 								END IF;
 							END IF;
 							MAR <= INSTRUCTIONS(PC);
-						WHEN "10" => 
+						WHEN "10" => --Comienza la ecuacion 3
 							IF (PC < 25) THEN
 								PC <= 26;
 							ELSE
@@ -219,17 +222,25 @@ BEGIN
 							pr_state <= state1;
 					END CASE;
 				WHEN state1 => -- DECODE
-					IF (MAR(11 DOWNTO 8) = "1101") THEN
+					IF (MAR(11 DOWNTO 8) = "1101") THEN --En caso que la instruccion sea load
 						REG_D <= "000000"&values(to_integer(unsigned(MAR(3 DOWNTO 0))));
-					ELSIF (MAR(11 DOWNTO 8) = "1110") THEN
-						IF(MAR(7 DOWNTO 4) /= reggy(7)(3 downto 0)) THEN
-							PC_AUX <= to_integer(unsigned(jmps(to_integer(unsigned(MAR(3 DOWNTO 0)))))) - 1;
-						ELSIF(MAR(7 DOWNTO 4) = "0000") THEN
-							PC_AUX <= to_integer(unsigned(jmps(to_integer(unsigned(MAR(3 DOWNTO 0)))))) - 1;
-						ELSE
+					
+					ELSIF (MAR(15 DOWNTO 12) = "1110") THEN --En caso que la instruccion sea un salto
+						--Aqui vamos a tener que hacer varias comparaciones
+						
+						IF(cmprs(to_integer(unsigned(MAR(11 DOWNTO 8)))) /= reggy(7)) THEN -- Primero se comprueba si hubo una comparacion primero (En este caso no se consideran las flags)
+							PC_AUX <= to_integer(unsigned(MAR(7 DOWNTO 0))) - 1;
+						
+						ELSIF(cmprs(to_integer(unsigned(MAR(11 DOWNTO 8)))) = "0000000000000000") THEN -- Si no hay comparacion se hace un salto directo
+							PC_AUX <= to_integer(unsigned(MAR(7 DOWNTO 0))) - 1;
+						
+						ELSE -- En cualquier otro caso no se hace nada
 							PC_AUX <= PC;
+						
 						END IF;
+					
 					ELSE
+						-- El resto de operaciones son controladas por la ALU
 						OP <= MAR(11 DOWNTO 8);
 						REG_A <= reggy(to_integer(unsigned(MAR(7 DOWNTO 4))));
 						s_p1 <= REG_A;
@@ -238,12 +249,16 @@ BEGIN
 					END IF;
 					pr_state <= state2;
 				WHEN state2 => -- EXECUTE
+				
 					IF (MAR(11 DOWNTO 8) = "1101") THEN
 						reggy(to_integer(unsigned(MAR(7 DOWNTO 4)))) <= "000000" & REG_D(9 downto 0);
-					ELSIF(MAR(11 DOWNTO 8) = "1110") THEN
-						PC <= PC_AUX; 
+						
+					ELSIF(MAR(15 DOWNTO 12) = "1110") THEN
+						PC <= PC_AUX; -- El salto se hace indicando el numero de instruccion
+					
 					ELSIF(MAR(11 DOWNTO 8) = "1011") THEN
-						reggy(7) <= MBR;
+						reggy(7) <= MBR; -- Aqui se guarda en el ultimo registro el resultado de la comparacion
+					
 					ELSE
 						IF (sflag = '1') THEN
 							reggy(to_integer(unsigned(MAR(7 DOWNTO 4)))) <= '1' & MBR(14 DOWNTO 0);
