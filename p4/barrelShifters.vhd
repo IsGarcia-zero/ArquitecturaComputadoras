ddca98b2e2893f1d7f00bac911fd496949b4e117
Eduardo Hernandez Vergara <61425144+IsGarcia-zero@users.noreply.github.com>
2022-12-20 11:18:47 -0600

    Hola

--- p4/barrelShifters.vhd
+++ p4/barrelShifters.vhd
@@ -4,7 +4,7 @@ USE IEEE.numeric_std.ALL;
 use IEEE.std_logic_unsigned.all;
 ENTITY barrelShifters IS
     PORT (
-        a : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        a, b : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
         cntrl, clk, iniciar : IN STD_LOGIC;
         salShifters : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
         salPrub : OUT STD_LOGIC
@@ -13,22 +13,34 @@ END ENTITY barrelShifters;
 
 ARCHITECTURE shifters OF barrelShifters IS
     SIGNAL aux : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL aux2 : INTEGER;
     --signal i: INTEGER RANGE 0 TO 3 := 0;
+    aux2 <= TO_INTEGER(unsigned(b));
 BEGIN
     --Las opciones del shifter
     Shifter : PROCESS (clk, cntrl, iniciar)
+    -- Rotacional a la izquierda que cambia aux2 veces
+    variable auxvar: STD_LOGIC_VECTOR(15 DOWNTO 0);
+    variable auxa: STD_LOGIC_VECTOR(15 DOWNTO 0);
     BEGIN
+        auxa := a;
         IF iniciar = '0' THEN
             aux <= "0000000000000000";
         ELSIF (rising_edge(clk)) THEN
             IF (cntrl = '0') THEN
-                --LSL
-                aux <= a(14 DOWNTO 0) & '0';
+                -- Rotacional a la izquierda
+                for i in 0 to aux2 loop
+                    auxa := auxa(14 DOWNTO 0) & auxa(15);
+                end loop;
+                --aux <= a(14 DOWNTO 0) & a(15);
                 --salShifters <= aux(2 DOWNTO 0);
             ELSE
-                aux <= a(15) & a(15 DOWNTO 1); --ASR
+                for i in 0 to aux2 loop
+                    auxa := auxa(15) & auxa(15 DOWNTO 1);
+                end loop;
+                --aux <= a(15) & a(15 DOWNTO 1); --ASR
             END IF;
-            salShifters <= aux;
+            salShifters <= auxa;
         END IF;
     END PROCESS Shifter;
     salPrub <= clk;
